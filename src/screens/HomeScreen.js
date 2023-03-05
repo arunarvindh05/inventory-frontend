6462d71007db8470975cbe7aad178a0eb6d51f1b
arunarvindh05 <arunarvindh05@gmail.com>
2023-03-05 21:20:09 +0530

    All codes together

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Col, Row } from 'react-bootstrap'
+import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { Link, useParams, useNavigate } from "react-router-dom";
+import Paginate from '../components/Paginate'
+
+const HomeScreen = () => {
+  const { keyword } = useParams()
+  const { pageNumber } = useParams() || 1
+
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products,page, pages } = productList
+
+  useEffect(() => {
+    dispatch(listProducts(keyword, pageNumber))
+
+
+  }, [dispatch, keyword, pageNumber])
+  return (
+    <>
+        <h1>Latest Products</h1>
+        {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+        <Paginate
+          pages={pages}
+          page={page}
+          keyword={keyword ? keyword : ''}
+        />
+      </>
+      )}
+    </>
+  )
+}
+
+export default HomeScreen
